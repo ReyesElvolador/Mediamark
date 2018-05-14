fe828a7c9fa8a1c2fc7ff175821aae786f660ace
reyes <xpepexpepa1@gmail.com>
2018-05-14 09:55:45 +0200

    Esqueleto inicial de la aplicación de base de datos en java

--- src/mediamarkt/MediaMarkt.java
+++ src/mediamarkt/MediaMarkt.java
@@ -1,20 +1,18 @@
-
 package mediamarkt;
-import java.sql.*;
 
+import java.sql.*;
 
 public class MediaMarkt {
 
-    public static void conexion(){
-    
-    
-     Connection conexion = null;
+    public static void conexion() {
+
+        Connection conexion = null;
         try {
-            // Cargar el driver, necesario añadir "mysql-connector-java-5.1.35-bin.jar" en la Biblioteca
+            // Cargar el driver xd, necesario añadir "mysql-connector-java-5.1.35-bin.jar" en la Biblioteca
             //del proyecto
             Class.forName("com.mysql.jdbc.Driver");
 
-            // Se obtiene una conexión con la base de datos.
+            // Se obtiene una conexión con la base de datos ok.
             // En este caso nos conectamos a la base de datos prueba
             // con el usuario root y contraseña 1daw
             conexion = DriverManager.getConnection("jdbc:mysql://localhost/mediamarkt", "root", "");
@@ -27,16 +25,15 @@ public class MediaMarkt {
 
             // Se recorre el ResultSet, mostrando por pantalla los resultados.
             while (rs.next()) {
-                System.out.println(rs.getInt("idproductos")+rs.getString("nombre") + " " + rs.getString(2) + " " + rs.getInt(3));
+                System.out.println(rs.getInt("idproductos") + rs.getString("nombre") + " " + rs.getString(2) + " " + rs.getInt(3));
             }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
-       } catch (ClassNotFoundException e) {
+        } catch (ClassNotFoundException e) {
             System.out.println(e.getMessage());
-       } catch (Exception e){
-        System.out.println(e.getMessage());
-        }
-    finally { // Se cierra la conexión con la base de datos.
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally { // Se cierra la conexión con la base de datos.
             try {
                 if (conexion != null) {
                     conexion.close();
@@ -46,10 +43,9 @@ public class MediaMarkt {
             }
         }
     }
-    
-    
+
     public static void main(String[] args) {
-       
-    conexion();
-}
+
+        conexion();
+    }
 }
